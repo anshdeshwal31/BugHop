32a42a1ef3c83178a765b1ca55a835a9325579d6
anshdeshwal31 <anshdeshwal0011@gmail.com>
2026-05-15 22:18:26 +0530

    fix the build of the frontend

--- frontend/app/api/user-by-installation/[installationId]/route.ts
+++ frontend/app/api/user-by-installation/[installationId]/route.ts
@@ -10,7 +10,7 @@ export async function GET(
 
     const installation = await prisma.installation.findUnique({
       where: {
-        installationId: installationId,
+        installationId: parseInt(installationId, 10),
       },
       select: {
         userId: true,
